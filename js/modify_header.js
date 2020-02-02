ca63d877edea42c50ddcdd7dced15b5b45b1741c
ning.wang <wayne5ning@hotmail.com>
2020-02-02 19:27:48 +0800

    minor fix of text

--- js/modify_header.js
+++ js/modify_header.js
@@ -1,5 +1,4 @@
 (function(){
-    window.onload = function(){
     let div = document.createElement("div");
     let txt = document.createTextNode("NING WANG'S WORK SAMPLES");
     div.appendChild(txt);
@@ -8,5 +7,4 @@
     if (h && h[0]){
         h[0].parentNode.prepend(div);
     }
-};
 }());
